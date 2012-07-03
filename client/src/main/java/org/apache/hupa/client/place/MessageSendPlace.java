210b91aad6b4038c0f496009206d715f11ebc6d0
echowdx@gmail.com <echowdx@gmail.com@14226458-c3ec-645b-2b44-f65113ec66be>
2012-07-03 17:52:13 +0000

    1. improve the inbox folder place. 2. commit all of the activities except contacts.
    git-svn-id: https://svn.codespot.com/a/apache-extras.org/hupa-evo/trunk@23 14226458-c3ec-645b-2b44-f65113ec66be

--- client/src/main/java/org/apache/hupa/client/place/MessageSendPlace.java
+++ client/src/main/java/org/apache/hupa/client/place/MessageSendPlace.java
@@ -1,27 +1,74 @@
 package org.apache.hupa.client.place;
 
+import org.apache.hupa.client.activity.MessageSendActivity.Type;
+import org.apache.hupa.shared.data.IMAPFolder;
+import org.apache.hupa.shared.data.Message;
+import org.apache.hupa.shared.data.MessageDetails;
+import org.apache.hupa.shared.data.User;
+
 import com.google.gwt.place.shared.Place;
 import com.google.gwt.place.shared.PlaceTokenizer;
 import com.google.gwt.place.shared.Prefix;
 
 public class MessageSendPlace extends Place {
 
-  @Prefix("MessageSend")
-  public static class Tokenizer implements PlaceTokenizer<MessageSendPlace> {
+	
+	private User user ;
+	private IMAPFolder folder;
+	private Message message;
+	private MessageDetails messageDetails;
+	private Type forward;
+	
+	
+	@Prefix("send")
+	public static class Tokenizer implements PlaceTokenizer<MessageSendPlace> {
 
-    @Override
-    public MessageSendPlace getPlace(String token) {
-      return new MessageSendPlace();
-    }
+		@Override
+		public MessageSendPlace getPlace(String token) {
+			return new MessageSendPlace();
+		}
 
-    @Override
-    public String getToken(MessageSendPlace place) {
-      return "MessageSend";
-    }
-  }
-  
-  public String toString(){
-	  return this.getClass().getName()+"->[MessageSend]";
-  }
+		@Override
+		public String getToken(MessageSendPlace place) {
+			return place.getForward().toString();
+		}
+	}
 
+	public String toString() {
+		return this.getClass().getName() + "->[MessageSend]";
+	}
+
+	public Place with(User user, IMAPFolder folder, Message message, MessageDetails messageDetails, Type forward) {
+		this.forward = forward;
+		this.user = user;
+		this.folder = folder;
+		this.message = message;
+		this.messageDetails = messageDetails;
+		return this;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public IMAPFolder getFolder() {
+		return folder;
+	}
+
+	public Message getMessage() {
+		return message;
+	}
+
+	public MessageDetails getMessageDetails() {
+		return messageDetails;
+	}
+
+	public Type getForward() {
+		return forward;
+	}
+	
+	
+
+	
+	
 }
