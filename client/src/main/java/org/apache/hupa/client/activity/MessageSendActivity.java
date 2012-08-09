9388dd081982c4c6568e23ea8cd05f7931a923a7
echowdx@gmail.com <echowdx@gmail.com@14226458-c3ec-645b-2b44-f65113ec66be>
2012-08-09 16:16:37 +0000

    remove both of gwt-representer and gwt-dispatch dependencies, add license headers to all new files
    git-svn-id: https://svn.codespot.com/a/apache-extras.org/hupa-evo/trunk@75 14226458-c3ec-645b-2b44-f65113ec66be

--- client/src/main/java/org/apache/hupa/client/activity/MessageSendActivity.java
+++ client/src/main/java/org/apache/hupa/client/activity/MessageSendActivity.java
@@ -1,3 +1,22 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
 package org.apache.hupa.client.activity;
 
 import gwtupload.client.IUploadStatus.Status;
@@ -18,7 +37,6 @@ import org.apache.hupa.client.ui.WidgetDisplayable;
 import org.apache.hupa.client.validation.EmailListValidator;
 import org.apache.hupa.shared.Util;
 import org.apache.hupa.shared.data.MessageAttachmentImpl;
-import org.apache.hupa.shared.data.SmtpMessageImpl;
 import org.apache.hupa.shared.domain.GenericResult;
 import org.apache.hupa.shared.domain.ImapFolder;
 import org.apache.hupa.shared.domain.Message;
