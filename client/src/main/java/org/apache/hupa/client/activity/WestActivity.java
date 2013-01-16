c242710f66107a146d30ca2989f27d075251acd0
echowdx@gmail.com <echowdx@gmail.com@14226458-c3ec-645b-2b44-f65113ec66be>
2013-01-16 14:55:16 +0000

    fix the bug that there will throw a NullPointerException after the first time into the message view page and then click the back link given not click the folders names
    git-svn-id: https://svn.codespot.com/a/apache-extras.org/hupa-evo/trunk@119 14226458-c3ec-645b-2b44-f65113ec66be

--- client/src/main/java/org/apache/hupa/client/activity/WestActivity.java
+++ client/src/main/java/org/apache/hupa/client/activity/WestActivity.java
@@ -26,6 +26,7 @@ import org.apache.hupa.client.place.MessageSendPlace;
 import org.apache.hupa.client.rf.GetMessageDetailsRequest;
 import org.apache.hupa.client.ui.WidgetDisplayable;
 import org.apache.hupa.client.widgets.IMAPTreeItem;
+import org.apache.hupa.shared.data.ImapFolderImpl;
 import org.apache.hupa.shared.data.MessageImpl.IMAPFlag;
 import org.apache.hupa.shared.domain.GetMessageDetailsAction;
 import org.apache.hupa.shared.domain.GetMessageDetailsResult;
@@ -75,6 +76,7 @@ public class WestActivity extends AppBaseActivity {
 	public WestActivity with(MailFolderPlace place) {
 		this.currentPlace = place;
 		this.user = place.getUser();
+		this.folder = new ImapFolderImpl(user.getSettings().getInboxFolderName());
 		return this;
 	}
 
