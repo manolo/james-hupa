673695bf5502b79421c771bd0a843325db69fe0e
wangdx <echowdx@gmail.com>
2013-07-16 16:55:02 +0800

    add top Loading... bar when expanding a message

--- client/src/main/java/org/apache/hupa/client/activity/MessageListActivity.java
+++ client/src/main/java/org/apache/hupa/client/activity/MessageListActivity.java
@@ -76,6 +76,7 @@ public class MessageListActivity extends AppBaseActivity {
 			@Override
 			public void onCellPreview(final CellPreviewEvent<Message> event) {
 				if (hasClickedButFirstCol(event)) {
+					hc.showTopLoading("Loading...");
 					antiSelectMessages(display.getGrid().getVisibleItems());
 					GetMessageDetailsRequest req = rf.messageDetailsRequest();
 					GetMessageDetailsAction action = req.create(GetMessageDetailsAction.class);
@@ -99,6 +100,7 @@ public class MessageListActivity extends AppBaseActivity {
 									+ event.getValue().getUid());
 							pc.goTo(place);
 							display.refresh();
+							hc.hideTopLoading();
 							eventBus.fireEvent(new RefreshUnreadEvent());
 						}
 
@@ -109,6 +111,7 @@ public class MessageListActivity extends AppBaseActivity {
 								// TODO write the error message to
 								// status bar.
 								toolBar.enableAllTools(false);
+								hc.hideTopLoading();
 								throw new RuntimeException(error.getMessage());
 							}
 						}
